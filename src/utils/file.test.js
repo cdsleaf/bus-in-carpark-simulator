fc2a99dde36cdbf8606c88427c58823b466c0dd5
cdsleaf <cdsleaf@gmail.com>
2019-01-27 15:37:19 +1300

    test: add input data in input.txt

--- src/utils/file.test.js
+++ src/utils/file.test.js
@@ -1,7 +1,10 @@
 import { loadFile } from './file';
 
 test('should load a input file.', () => {
-  const expectedObject = [['PLACE', '0,0,NORTH'], ['MOVE'], ['REPORT']];
+  const expectedObject = [["PLACE", "0,0,NORTH"], ["MOVE"], ["REPORT"], 
+    ["PLACE", "0,0,NORTH"], ["LEFT"], ["REPORT"], ["PLACE", "1,2,EAST"], 
+    ["MOVE"], ["MOVE"], ["LEFT"], ["MOVE"], ["REPORT"], ["PLACE", "5,5,SOUTH"], 
+    ["MOVE"], ["MOVE"], ["REPORT"]];
 
   expect(loadFile('input.txt')).toMatchObject(expectedObject);
 })
