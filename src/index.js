c451a1a9a43f70015d6e690d274f563f04a97469
cdsleaf <cdsleaf@gmail.com>
2019-01-27 23:03:41 +1300

    fix: alter the path of the import of carpark.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import { loadFile } from './utils/file';
-import Carpark from './carpark';
+import Carpark from './bus/carpark';
 import Bus from './bus/bus';
 import Process from './command/process';
 
 const carpark = new Carpark(5,5);
 const bus = new Bus();
-const processInCarpark = new Process(carpark.dimension);
+const processInCarpark = new Process(carpark);
 
 const inputtedCommands = loadFile('input.txt');
 
