ebef9991909f29e49807e3aa55d208230cfe734b
cdsleaf <cdsleaf@gmail.com>
2019-01-27 17:11:08 +1300

    test: add integration test cases

---
+++ test/integration.test.js
@@ -0,0 +1,33 @@
+import { loadFile } from '../src/utils/file';
+import Bus from '../src/bus/bus';
+
+test('Typical cases', () => {
+  const expected = {
+    x: 3, y: 4, direction: 'NORTH'
+  }
+  const newBus = new Bus(5,5);
+  const inputtedCommands = loadFile('./test/typicalCase.txt');
+  newBus.processCommands(inputtedCommands);
+  expect(newBus.currentPosition).toMatchObject(expected);
+})
+
+test('commands without PLACE cases', () => {
+  const expected = {
+    x: null, y: null, direction: null
+  }
+  const newBus = new Bus(5,5);
+  const inputtedCommands = loadFile('./test/commandsWithoutPlaceCase.txt');
+  newBus.processCommands(inputtedCommands);
+  expect(newBus.currentPosition).toMatchObject(expected);
+})
+
+test('too many move commands - 0,0,NORTH -> 10 MOVES -> RIGHT -> MOVE', () => {
+  const expected = {
+    x: 1, y: 4, direction: 'EAST'
+  }
+  const newBus = new Bus(5,5);
+  const inputtedCommands = loadFile('./test/tooManyMoveCommands.txt');
+  newBus.processCommands(inputtedCommands);
+  expect(newBus.currentPosition).toMatchObject(expected);
+})
+
