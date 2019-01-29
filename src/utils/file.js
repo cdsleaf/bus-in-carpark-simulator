30ee671166aec32819d099f553d5c420af4cc72b
cdsleaf <cdsleaf@gmail.com>
2019-01-29 23:31:33 +1300

    fix: Change the newline character to LF in file.js

--- src/utils/file.js
+++ src/utils/file.js
@@ -4,7 +4,7 @@ import fs from 'fs';
 export function loadFile(fileName){
   try {
     return fs.readFileSync(fileName, 'utf8')
-      .split('\r\n')
+      .split('\n')
       .map(n=>n.split(' '))
       .reduce((a,v) => {
         return v[0] === ''
